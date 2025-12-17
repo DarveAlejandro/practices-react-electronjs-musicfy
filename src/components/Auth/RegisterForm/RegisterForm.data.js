f0c481664d0bba93cbe7d73c2722e73f52585b98
Darve Alejandro Alva Blanco <alex.alvablanco@gmail.com>
2025-12-17 16:13:35 -0600

    Install formik && send request RegisterForm data

---
+++ src/components/Auth/RegisterForm/RegisterForm.data.js
@@ -0,0 +1,13 @@
+// -------------------------------------------------------
+// Config Formik Form Fields ( RegisterForm.data.js)
+// --------------------------------------------------------
+
+export function initialValues(){
+
+    return {
+
+        email: "@gmail.com",
+        password: "",
+        username: "",
+    }
+}
--- src/components/Auth/RegisterForm/RegisterForm.js
+++ src/components/Auth/RegisterForm/RegisterForm.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import { Form, Icon } from "semantic-ui-react";
 import "./RegisterForm.scss";
+import { useFormik } from "formik";
+import { initialValues } from './RegisterForm.data';
+
 
 export default function RegisterForm( props ) {
 
@@ -10,6 +13,15 @@ export default function RegisterForm( props ) {
 
   const { goBack, openLogin } = props; // Sistema basico de navegación sin uso de momento
 
+  const formik = useFormik({
+    initialValues: initialValues(),
+    onSubmit: (formValue) => {
+
+      console.log('Registro Ok');
+      console.log(formValue);
+    }
+  })
+
   // -----------------------------------------------------------
   // Atributes && Methods.
   // -----------------------------------------------------------  
@@ -17,22 +29,32 @@ export default function RegisterForm( props ) {
     
     <div className="register-form">
       <h1>¡Empieza a escuchar con una cuenta de Musicfy Gratis!</h1>
-      <Form>
+      <Form onSubmit={formik.handleSubmit}>
         <Form.Input className="form-field"
+          name="email"
           type="text"
+          value={formik.values.email}
+          onChange={formik.handleChange}
           placeholder="Correo Electronico" 
           icon="mail outline"
           error={true}
+          
         />
         <Form.Input className="form-field"
+          name="password"
           type="password"
           placeholder="Contraseña" 
           icon={<Icon name="eye" link onClick={ () => console.log("Show Password") }/>}
+          value={formik.values.password}
+          onChange={formik.handleChange}
         />
         <Form.Input className="form-field"
+          name="username"
           type="text"
           placeholder="¿Como deberiamos llamarte?" 
           icon="user circle outline"
+          value={formik.values.username}
+          onChange={formik.handleChange}
         />
         <Form.Button type="submit" primary className="form-field" fluid>
             Continuar
